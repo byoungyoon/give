7100e06552554b15c6208b611ffc098f5bef12c5
byoungyoon <bur5698@naver.com>
2024-09-05 15:41:08 +0900

    :art: animation 업데이트

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@
 import styles from './page.module.css';
 import Header from './image/header.png';
 import { useEffect, useMemo, useState } from 'react';
-import MotionText from '@/app/_component/MotionText';
 import Image from 'next/image';
-import { AnimatePresence, motion } from 'framer-motion';
-
-const MOTION_LAYOUT = 'layout';
+import Date from './image/date.png';
+import { motion } from 'framer-motion';
+import { FaAngleDoubleDown } from 'react-icons/fa';
 
 export default function Home() {
   const [timer, setTimer] = useState(0);
@@ -36,50 +35,54 @@ export default function Home() {
 
   return (
     <section className={styles.section}>
-      <AnimatePresence>
-        {!hasContent && (
-          <motion.article
-            className={styles.centerArticle}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.9 }}
-          >
-            <div className={styles.centerTextGroup}>
-              {centerText.map(
-                (text, index) =>
-                  timer === index && (
-                    <MotionText
-                      key={index}
-                      text={text}
-                      layoutId={MOTION_LAYOUT}
-                    />
-                  ),
-              )}
-            </div>
-          </motion.article>
-        )}
-      </AnimatePresence>
-      {hasContent && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.9, delay: 0.9 }}
-        >
-          <header className={styles.header}>
-            <Image src={Header} alt='header' className={styles.headerImage} />
-            <div>
-              <h3 className={styles.headerTitle}>
-                당신의 여행을 <strong>응원</strong>합니다.
-              </h3>
-              <h3 className={styles.headerTitle}>
-                꼭 <strong>살아서</strong> 돌아오세요.
-              </h3>
-            </div>
-          </header>
-        </motion.div>
-      )}
+      {/*<AnimatePresence>*/}
+      {/*  {!hasContent && (*/}
+      {/*    <motion.article*/}
+      {/*      className={styles.centerArticle}*/}
+      {/*      initial={{ opacity: 0 }}*/}
+      {/*      animate={{ opacity: 1 }}*/}
+      {/*      exit={{ opacity: 0 }}*/}
+      {/*      transition={{ duration: 0.9 }}*/}
+      {/*    >*/}
+      {/*      <div className={styles.centerTextGroup}>*/}
+      {/*        {centerText.map(*/}
+      {/*          (text, index) =>*/}
+      {/*            timer === index && (*/}
+      {/*              <MotionText*/}
+      {/*                key={index}*/}
+      {/*                text={text}*/}
+      {/*                layoutId={MOTION_LAYOUT}*/}
+      {/*              />*/}
+      {/*            ),*/}
+      {/*        )}*/}
+      {/*      </div>*/}
+      {/*    </motion.article>*/}
+      {/*  )}*/}
+      {/*</AnimatePresence>*/}
+      {/*{hasContent && (*/}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.9, delay: 0.9 }}
+      >
+        <header className={styles.header}>
+          <Image src={Header} alt='header' className={styles.headerImage} />
+          <div>
+            <h3 className={styles.headerTitle}>
+              당신의 여행을 <strong>응원</strong>합니다.
+            </h3>
+            <h3 className={styles.headerTitle}>
+              꼭 <strong>살아서</strong> 돌아오세요.
+            </h3>
+          </div>
+        </header>
+        <div className={styles.body}>
+          <Image src={Date} alt='date' className={styles.date} />
+          <FaAngleDoubleDown className={styles.arrow} size={25} />
+        </div>
+      </motion.div>
+      {/*)}*/}
     </section>
   );
 }
